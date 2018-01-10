a908c8f56b7679a5df78e60c87ad343f53ecad7a
C?dric Krier <ced@b2ck.com>
2018-01-10 11:07:02 +0100

    Update to 4.7

--- __init__.py
+++ __init__.py
@@ -1,13 +1,14 @@
-#This file is part account_invoice_jreport module for Tryton.
-#The COPYRIGHT file at the top level of this repository contains
-#the full copyright notices and license terms.
+# This file is part account_invoice_jreport module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains
+# the full copyright notices and license terms.
 from trytond.pool import Pool
-from .account_invoice import *
+from . import account_invoice
+
 
 def register():
     Pool.register(
-        Invoice,
+        account_invoice.Invoice,
         module='account_invoice_jreport', type_='model')
     Pool.register(
-        InvoiceReport,
+        account_invoice.InvoiceReport,
         module='account_invoice_jreport', type_='report')
